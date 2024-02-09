97e1e38fc778742ac1e4a703977d136055b2e0f5
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-02-09 14:49:04 +0600

    Quick_sort.cpp

---
+++ Quick_sort.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int a[], int l, int h) {
+    int pivot = a[l];
+    int i = l;
+    int j = h;
+    while (i < j) {
+        while (a[i] <= pivot) {
+            i++;
+        }
+        while (a[j] > pivot) {
+            j--;
+        }
+        if (i < j) {
+            swap(a[i], a[j]);
+        }
+    }
+  // when i>j
+    swap(a[l], a[j]);
+    return j; //here we return the index number of j,because in this condition the pivot find it's right position and we need to sort left and right side also so we have to know the index
+}
+
+void quicksort(int a[], int l, int h) {
+    if (l < h) {
+        int j = partition(a, l, h);
+      /*
+      when pivot find it's position then we need to sort a[0] to pivot-1,hence we find the j index so we use j-1
+      */
+        quicksort(a, l, j - 1); 
+       /*
+      when pivot find it's position then we need to sort a[j+1] to n,hence we find the j index so we use j+11
+      */
+        quicksort(a, j + 1, h);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    quicksort(a, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
