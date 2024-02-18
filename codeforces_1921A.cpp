a653519dda5b7aa83df58b823c92725a5f3f1765
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2024-02-18 23:07:57 +0600

    Update codeforces_1921A.cpp

--- codeforces_1921A.cpp
+++ codeforces_1921A.cpp
@@ -3,21 +3,21 @@
  
 int main() {
     int t;
-    cin >>t;
+ std::cin >>t;
  
     while (t--) {
         int x[4], y[4];
  
         // Input the coordinates of the corners
         for (int i = 0; i < 4; ++i) {
-            cin >> x[i] >> y[i];
+          std::cin >> x[i] >> y[i];
         }
  
         // Calculate the side length of the square
         int side_length = fmax(abs(x[0] - x[1]), abs(y[0] - y[1]));
  
         // Output the area of the square
-        cout << side_length * side_length << endl;
+       std:: cout << side_length * side_length << std::endl;
     }
  
     return 0;
